cf974d6ab3371666196a1023ba85d0a2513b8a86
Paul Singh <hey@paulsingh.dev>
2024-08-21 20:27:48 +0100

    :bug: Fix build error resulting from top level await

--- app/lib/prisma/client.ts
+++ app/lib/prisma/client.ts
@@ -3,13 +3,15 @@ import { PrismaClient } from "@prisma/client";
 // init var to essentially cache the client
 let prisma: PrismaClient | undefined = undefined;
 
-// Not defined
-if (!prisma) {
-  // Define it
-  prisma = new PrismaClient();
+(async () => {
+  // Not defined
+  if (!prisma) {
+    // Define it
+    prisma = new PrismaClient();
 
-  // Connect to DB
-  await prisma.$connect();
-}
+    // Connect to DB
+    await prisma.$connect();
+  }
+})();
 
 export { prisma };
